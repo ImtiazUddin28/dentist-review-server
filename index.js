7e7c66caf471a51c8240cc6ebe7ecaa3a769b3f3
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-11-09 15:53:42 +0600

    some server routes are added

--- index.js
+++ index.js
@@ -10,15 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d48h4e6.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run() {
     try {
-        const serviceCollection = client.db('geniusCar').collection('services');
-        const orderCollection = client.db('geniusCar').collection('orders');
+        const serviceCollection = client.db('dentist-review').collection('services');
+        const orderCollection = client.db('dentist-review').collection('reviews');
 
+        app.get('/homeservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query);
