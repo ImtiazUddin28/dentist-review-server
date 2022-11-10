48e0815344a168db4427f94bb4033e063628291f
ImtiazUddin28 <imtiaz100463@gmail.com>
2022-11-10 09:09:00 +0600

    service commit method is added

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ async function run() {
             res.send(service);
         });
 
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        });
 
         app.get('/reviews', async (req, res) => {
             let query = {};
